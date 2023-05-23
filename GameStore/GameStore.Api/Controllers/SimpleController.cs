12c7f7968dae18228a070b1be530f94269fc9bd8
andreleaos <andreleaos@gmail.com>
2023-05-23 00:34:11 -0300

    Exemplo de chamada http Web App a Api e exibicao de Imagem em pagina

---
+++ GameStore/GameStore.Api/Controllers/SimpleController.cs
@@ -0,0 +1,44 @@
+﻿using GameStore.Domain.Entities;
+using GameStore.Domain.Enums;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimpleController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            try
+            {
+                Produto produto = GetProductExample();
+                return Ok(produto);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private Produto GetProductExample()
+        {
+            string url = @"C:\Repo\LojaGames\GameStore\GameStore.Infrastructure\Images\Fifa23.png";
+
+            ImagemProduto imagem = new ImagemProduto(1, url);
+
+            Produto produto = new Produto()
+            {
+                Id = 1,
+                Descricao = "Fifa 23 PS4",
+                PrecoUnitario = 79.90,
+                Categoria = CategoriaProduto.Game,
+                ImagemProduto = imagem
+            };
+
+            return produto;
+        }
+    }
+}
---
+++ GameStore/GameStore.Web/Controllers/SimpleController.cs
@@ -0,0 +1,38 @@
+﻿using GameStore.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace GameStore.Web.Controllers
+{
+    public class SimpleController : Controller
+    {
+        private HttpClient _httpClient = null;
+        private string _url_base_address = string.Empty;
+
+        public SimpleController(IConfiguration configuration)
+        {
+            _url_base_address = configuration.GetSection("Endpoints").GetValue<string>("Api");
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                string endpointApi = $"{_url_base_address}api/simple";
+
+                _httpClient.BaseAddress = new Uri(_url_base_address);
+                HttpResponseMessage response = await _httpClient.GetAsync(endpointApi);
+
+                var content = response.Content.ReadAsStringAsync().Result;
+                var result = JsonConvert.DeserializeObject<Produto>(content);
+                return View(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
