e5102f457bbcf44fb56cedc1beb90f879e833de3
andreleaos <andreleaos@gmail.com>
2023-05-24 12:05:27 -0300

    Implementacao da camada de servico

---
+++ GameStore/GameStore.Infrastructure/Services/IProdutoService.cs
@@ -0,0 +1,15 @@
+﻿using GameStore.Domain.Entities;
+using GameStore.Infrastructure.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Infrastructure.Services
+{
+    public interface IProdutoService : IGenericRepoService<Produto, int>
+    {
+
+    }
+}
---
+++ GameStore/GameStore.Infrastructure/Services/ProdutoService.cs
@@ -0,0 +1,45 @@
+﻿using GameStore.Domain.Entities;
+using GameStore.Infrastructure.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Infrastructure.Services
+{
+    public class ProdutoService : IProdutoService
+    {
+        private readonly IProdutoRepository _repository;
+
+        public ProdutoService(IProdutoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Create(Produto produto)
+        {
+            _repository.Create(produto);
+        }
+
+        public bool Delete(int id)
+        {
+            return _repository.Delete(id);
+        }
+
+        public List<Produto> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public Produto? GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public void Update(Produto entity)
+        {
+            _repository.Update(entity);
+        }
+    }
+}
