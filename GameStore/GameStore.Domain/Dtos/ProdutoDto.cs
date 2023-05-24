5991c8569e5e084a7085087b99bfe2d58f140b22
andreleaos <andreleaos@gmail.com>
2023-05-24 10:26:42 -0300

    Iniciado a implementacao da feature de cadastro de produtos

---
+++ GameStore/GameStore.Domain/Dtos/ProdutoDto.cs
@@ -0,0 +1,19 @@
+﻿using GameStore.Domain.Entities;
+using GameStore.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Domain.Dtos
+{
+    public class ProdutoDto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double PrecoUnitario { get; set; }
+        public CategoriaProduto Categoria { get; set; }
+        public string UrlProduto { get; set; }
+    }
+}
